84df49a95128f7790d99376e7fcbc6e2e73c48ef
AndrewzCode <opioandrew581@gmail.com>
2020-07-12 13:37:57 +0300

    server1

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const server = require("http").createServer(app);
+const io = require('socket.io').listen(server);
+const port = 3000;
+
+io.on("connection", socket => {
+    //recieving and sending messages
+    socket.on("message", msg =>{
+        io.emit(msg.email, msg.text, msg.type);
+    });
+    //emitted when a message is sent inorder to update chats
+    socket.on("sent", msg =>{
+        io.emit("sent"+ msg.email, msg.email);
+    });
+
+    //update messages from unread to read
+    socket.on("update", msg =>{  
+        io.emit("update"+ msg.email, msg.email);
+    });
+
+    //updating the profile pic after changing it
+     socket.on("propic", msg =>{
+        io.emit(msg.email, msg.text);
+    });
+
+    //sending notifications to users by district
+    socket.on("notification", msg =>{
+        io.emit("notification", msg);
+    });
+    
+    //Listening in to comments and updating the homepage
+    socket.on("comment", msg =>{
+        io.emit("comment"+msg.email, msg);
+    });
+});
+
+server.listen(port, () => console.log("server running on" + " " + port));
